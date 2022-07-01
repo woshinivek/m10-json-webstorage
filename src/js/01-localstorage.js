be78f216f299c3f640abac92c3c6e3d60fac12b7
woshinivek <woshinivek@yahoo.com>
2022-07-01 18:57:34 -0400

    started

--- src/js/01-localstorage.js
+++ src/js/01-localstorage.js
@@ -1,1 +1,31 @@
 import '../css/common.css';
+
+// const objA = {
+//   name: 'mango',
+//   age: 23,
+// };
+
+// const jsonObjA = JSON.stringify(objA);
+// console.log(jsonObjA);
+
+// const reversJson = JSON.parse(jsonObjA);
+// console.log(reversJson);
+
+// -----------------------------------------------------------------
+// -----------------------------------------------------------------
+
+console.log(localStorage);
+
+localStorage.setItem('name', 'mango');
+
+const objA = {
+  name: 'mango',
+  age: 23,
+};
+
+localStorage.setItem('user', JSON.stringify(objA));
+
+console.log(JSON.parse(localStorage.getItem('user')));
+
+// -----------------------------------------------------------------
+// -----------------------------------------------------------------
