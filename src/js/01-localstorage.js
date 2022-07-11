44dba4efc01738e8594a5c9968d06e15704c765a
woshinivek <woshinivek@yahoo.com>
2022-07-11 13:55:52 -0400

    randome

---
+++ src/js/01-localstorage.js
@@ -0,0 +1,34 @@
+import '../css/common.css';
+import vehicles from '../js/vehicles.js';
+
+class Car {
+  constructor({ make, model, type, price } = {}) {
+    this.make = make;
+    this.model = model;
+    this.type = type;
+    this.price = price;
+  }
+
+  setThePrice(newPrice) {
+    this.price = newPrice;
+  }
+}
+
+const nissan = new Car({
+  make: 'nissan',
+  model: 'Skyline GT-R',
+  type: 'Sedan',
+  price: 19099,
+});
+
+console.log(nissan);
+
+const toyota = vehicles
+  .filter(el => el.make === 'Toyota')
+  .map(el => new Car(el));
+
+console.log(toyota);
+
+const ford = vehicles.filter(el => el.make === 'Ford').map(el => new Car(el));
+
+console.log(ford);
